82e373cdee114fe68403431bd3bc56766b9bdea7
Eda Öney <oneeyyeda@gmail.com>
2025-07-28 18:57:33 +0300

    Completed

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("ÇARPIM TABLOSU");
+
+for (int i = 1; i <= 10; i++)
+{
+    for (int j = 1; j <= 10; j++)
+    {
+        Console.Write("{0}*{1}={2}\t", i, j, (i*j));
+    }
+    Console.WriteLine();
+}
